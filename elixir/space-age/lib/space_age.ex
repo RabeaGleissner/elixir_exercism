3993fcb8ea52b07daab454ec422efc407dedd770
Rabea Gleissner <rabeagleissner@gmail.com>
2021-05-02 10:40:12 +0800

    Improve code

--- elixir/space-age/lib/space_age.ex
+++ elixir/space-age/lib/space_age.ex
@@ -14,27 +14,27 @@ defmodule SpaceAge do
   aged on 'planet'.
   """
   @spec age_on(planet, pos_integer) :: float
+  @orbital_periods %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
 
   def age_on(planet, seconds) do
     calculate_age(planet, seconds)
   end
 
-  def calculate_age(:earth, seconds), do: seconds / 31557600
-  def calculate_age(planet, seconds) do
+  defp calculate_age(:earth, seconds), do: seconds / 31557600
+  defp calculate_age(planet, seconds) do
     calculate_age(:earth, seconds)
     |> age_for_planet(planet)
   end
 
-  def age_for_planet(earth_years, planet) do
-    orbital_periods = %{
-      mercury: 0.2408467,
-      venus: 0.61519726,
-      mars: 1.8808158,
-      jupiter: 11.862615,
-      saturn: 29.447498,
-      uranus: 84.016846,
-      neptune: 164.79132
-    }
-    earth_years / Map.get(orbital_periods, planet)
+  defp age_for_planet(earth_years, planet) do
+    earth_years / Map.get(@orbital_periods, planet)
   end
 end
